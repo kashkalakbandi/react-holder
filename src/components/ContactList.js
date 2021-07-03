cc0b89c78c67aec0da7a5d77b71ac6d2b0168ee6
Kashyap Kalakbandi <kkalakbandi@internal.salesforce.com>
2021-07-03 23:14:57 +0530

    working sample react app with firebase realtime database with basic add, fetch and delete operations

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,7 +4,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import firebase from '../utils/firebase';
-
+ import DeleteIcon from '@material-ui/icons/Delete';
+ import ContactsIcon from '@material-ui/icons/Call';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 // styling for list items
 const useStyles = makeStyles((theme) => ({
@@ -25,7 +27,23 @@ function ContactList(){
 const [renderList, setrenderList] = useState();  
 
 
+// deletion handler
+const deletionHandler = (e)=>{
+console.log(e.target.parentElement.id);
+const contactFromDB = firebase.database().ref("Contacts");
 
+// sort all children by the phone and check if something is equal to the target value 
+contactFromDB.orderByChild("phone").equalTo(e.target.parentElement.id).on('value',(snapshot)=>{
+snapshot.forEach((childsnapshot)=>{
+// console.log(childsnapshot.key);
+//  key is found on childsnapshot key.
+// get the reference and then remove it
+childsnapshot.ref.remove();
+
+});
+});
+
+}
 
   useEffect(() => {
     const contactReference = firebase.database().ref("Contacts");
@@ -60,7 +78,13 @@ const [renderList, setrenderList] = useState();
 <div className={classes.centerdiv}>
       <List className={classes.root}>
       {renderList ? renderList.map(
-        (contact)=>(<ListItem><ListItemText key={contact.id} primary={contact.name} secondary={contact.email} /></ListItem>)): ''}
+        (contact)=>(<ListItem>
+            
+          <DeleteIcon id={contact.phone} onClick={deletionHandler}/> &nbsp;&nbsp;&nbsp;
+       
+          <ListItemText key={contact.phone} primary={contact.name} secondary={'Contact :'+contact.email+' , '+contact.phone}  /></ListItem>
+          
+          )): ''}
         </List>
       </div>
     );
